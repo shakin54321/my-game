b7c8b516aedede6d7f0d5eeab7fbc1cdc15314c3
shakin54321 <shakinkhan4041@gmail.com>
2026-09-24 09:50:00 +0600

    Serve standalone game at homepage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports={
-  reactStrictMode:true,
-  async redirects(){
-    return [{source:"/",destination:"/neon",permanent:false}];
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [{ source: "/", destination: "/neon.html", permanent: false }];
   }
 };
