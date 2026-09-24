64af3b94f56c5ff659ad67aeff23a83543fd8617
shakin54321 <shakinkhan4041@gmail.com>
2026-09-24 10:00:51 +0600

    Remove homepage redirect causing route 404s

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [{ source: "/", destination: "/neon.html", permanent: false }];
-  }
-};
+module.exports = { reactStrictMode: true };
