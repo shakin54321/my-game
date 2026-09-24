819fef6c87fd1a263ab869be7719fd75cbe6b829
shakin54321 <shakinkhan4041@gmail.com>
2026-09-24 09:46:12 +0600

    Redirect homepage to Neon Rush 3D

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports={reactStrictMode:true};
+module.exports={
+  reactStrictMode:true,
+  async redirects(){
+    return [{source:"/",destination:"/neon",permanent:false}];
+  }
+};
